d4e539f34414b68ed1b54e1411aa8f1711d690cd
ChrisW-priv <chris.watras@gmail.com>
2020-08-12 20:25:36 +0200

    Sudoku

--- Games/Sudoku.py
+++ Games/Sudoku.py
@@ -1,51 +1,81 @@
-class Sudoku:
-	def __init__(self, matrix):
-		self.matrix = matrix
-		self.solve()
-
-	def solve(self):
-		for y in range(9):
-			for x in range(9):
-				if self.matrix[y][x] == 0:
-					for n in range(1,10):
-						if self.possible(y,x,n):
-							self.matrix[y][x] = n
-							self.solve()
-							self.matrix[y][x] = 0
-					return
-		print(self.matrix)
-
-	def possible(self, y,x,n):
-		for i in range(9):
-			if self.matrix[y][i] == n:
-				return False
-		for i in range(9):
-			if self.matrix[i][x] == n:
-				return False
-
-		x0 = (x//3)*3
-		y0 = (x//3)*3
-
-		for i in range(3):
-			for j in range(3):
-				if self.matrix[y0+1][x0+j] == n:
-					return False
-
-
-
-
-matrix = [
-	[0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0],
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
 ]
 
-sudoku = Sudoku(matrix)
+class Sudoku:
+	def __init__(self, board):
+		self.board = board
+	
+	def __repr__(self):
+	    str_grid = ""
+	    for i in range(len(self.board)):
+	        if i % 3 == 0 and i != 0:
+	            str_grid += ("- - - - - - - - - - - - ")
+
+	        for j in range(len(self.board[0])):
+	            if j % 3 == 0 and j != 0:
+	                str_grid += (" | ")
+
+	            if j == 8:
+	                str_grid += str((self.board[i][j]))
+	            else:
+	                str_grid += (str(self.board[i][j]) + " ")
+	    return str_grid
+
+	def solve(self):
+	    find = self.find_empty()
+	    if not find:
+	        return True
+	    else:
+	        row, col = find
+
+	    for i in range(1,10):
+	        if self.valid(i, (row, col)):
+	            self.board[row][col] = i
+
+	            if self.solve():
+	                return True
+
+	            self.board[row][col] = 0
+
+	    return False
+
+
+	def valid(self, num, pos):
+	    # Check row
+	    for i in range(len(self.board[0])):
+	        if self.board[pos[0]][i] == num and pos[1] != i:
+	            return False
+	    # Check column
+	    for i in range(len(self.board)):
+	        if self.board[i][pos[1]] == num and pos[0] != i:
+	            return False
+	    # Check box
+	    box_x = pos[1] // 3
+	    box_y = pos[0] // 3
+
+	    for i in range(box_y*3, box_y*3 + 3):
+	        for j in range(box_x * 3, box_x*3 + 3):
+	            if self.board[i][j] == num and (i,j) != pos:
+	                return False
+	    # default
+	    return True
+
+	def find_empty(self):
+	    for i in range(len(self.board)):
+	        for j in range(len(self.board[0])):
+	            if self.board[i][j] == 0:
+	                return (i, j)  # row, col
+	    return None
+
+sudoku = Sudoku(board)
 sudoku.solve()
-print(sudoku.matrix)
+print(sudoku)
