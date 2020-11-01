8deaf6790f3528d79606ce5d8df648259bb1e2d1
ChrisW-priv <chris.watras@gmail.com>
2020-11-01 22:27:16 +0100

    Minor MinMax changes to make it more clear

--- Games/MinMax.py
+++ Games/MinMax.py
@@ -1,7 +1,7 @@
 class Tile():
-	def __init__(self, lvl=0, value=None):
-		self.lvl = lvl
+	def __init__(self, value=None):
 		self.value = value
+		self.lvl = 0
 		self.next = []
 
 
@@ -10,8 +10,7 @@ tiles_to_do = [first_tile]
 
 MAX_LVL = 2
 BRANCHES = 3
-values_on_the_end = ( range(1, BRANCHES**MAX_LVL+1) )
-i=0
+values_on_the_end = ( range(1, BRANCHES**MAX_LVL+1) );i=0
 
 # create tree
 current_lvl = 0
@@ -20,11 +19,13 @@ while tiles_to_do:
 	current_lvl = top.lvl+1
 	for _ in range(BRANCHES):
 		if current_lvl == MAX_LVL:
-			new_tile = Tile(current_lvl, values_on_the_end[i])
+			new_tile = Tile(values_on_the_end[i])
+			new_tile.lvl = current_lvl
 			top.next.append(new_tile)
 			i+=1
 		else:
-			new_tile = Tile(current_lvl)
+			new_tile = Tile()
+			new_tile.lvl = current_lvl
 			top.next.append(new_tile)
 			tiles_to_do.append(new_tile)
 
