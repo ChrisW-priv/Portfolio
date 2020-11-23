7c56fd777ef1dfb8f6628d043acb069c95bef65e
Chris-priv <chris.watras@gmail.com>
2020-11-23 14:32:41 +0100

    major update! getting rid of bugs, cleaning code implementing mirror feature

--- Games/MinMaxFourInLine.py
+++ Games/MinMaxFourInLine.py
@@ -1,36 +1,44 @@
 import numpy as np
 from copy import deepcopy
-import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 
 class FourInLine:
 	def __init__(self, board=np.empty(shape=(7, 6), dtype=str)):
 		self.board = board
-		self.highest = [0, 0, 0, 0, 0, 0, 0]
-		self.last_move = (0, 0)
 		self.next = []
 		self.value = 0
-		self.lvl = 0
+		self.players = ['W', 'B']
+		self.current_player = self.players[self.lvl % 2]
 
 	def __repr__(self):
 		str_grid = "1   2   3   4   5   6   7\n-   -   -   -   -   -   -\n"
 		for y in range(6):
 			if y != 0:
-				str_grid += ("- - - - - - - - - - - - -\n")
+				str_grid += "- - - - - - - - - - - - -\n"
 			for x in range(7):
 				position = self.board[x][5-y]
 				if x % 1 == 0 and x != 0:
-					str_grid += (" | ")
+					str_grid += " | "
 				if position == '':
 					str_grid += ' '
 				if x == 6:
 					str_grid += position + '\n'
 				else:
 					str_grid += position
-		return str_grid
+		return f"{self.lvl}\n{self.highest}\n{str_grid}"
 
-	def demo(self):
-		return f"{self.lvl}\n{self.highest}\n{self.value}\n{self}"
+	@property
+	def is_symmetric(self):
+		return np.all(self.board == self.board[::-1])
+
+	@property
+	def highest(self):
+		return [6 - np.unique(v, return_counts=True)[1][0] for v in self.board]
+
+	@property
+	def lvl(self):
+		return sum(self.highest)
 
 	def there_is_a_winner(self):
 		if sum(self.highest) >= 7:
@@ -56,17 +64,20 @@ class FourInLine:
 			vertical_lines = [vertical_line(row, col) for row in range(4) for col in range(6)]
 			diagonal_right_lines = [diagonal_right_line(row, col) for row in range(4) for col in range(3)]
 			diagonal_left_lines = [diagonal_left_line(row, col) for row in range(4) for col in range(3, 6)]
-
 			return True in horizontal_lines + vertical_lines + diagonal_right_lines + diagonal_left_lines
 
-	def possible_moves(self, player):
+	def possible_moves(self):
 		moves = []
-		if self.found_direct_win(player):
+		nonsense_moves = []
+		if self.is_symmetric:
+			nonsense_moves = [4, 5, 6]
+		if self.found_direct_win(self.current_player):
 			self.value = 1
-			moves.append(self.found_direct_win(player))
+			moves.append(self.found_direct_win(self.current_player))
 		else:
-			for move, highest in enumerate(self.highest):
-				if highest < 5 and not self.move_leading_to_direct_loss(move, player):
+			for move in range(7):
+				if self.highest[move] < 6 and move not in nonsense_moves \
+						and not self.move_leading_to_direct_loss(move, self.current_player):
 					moves.append(move)
 		return moves
 
@@ -76,108 +87,67 @@ class FourInLine:
 				self.move(move, player)
 				if self.there_is_a_winner():
 					return move
-				self.move_back()
+				self.move_back(move)
 
 	def move_leading_to_direct_loss(self, move, player):
 		# check if opponent has a direct win if we do not prevent it
-		players = ['W', 'B']
-		opponent = players[players.index(player)-1]
+		opponent = self.players[self.players.index(player)-1]
 		self.move(move, player)
 		if_found = self.found_direct_win(opponent)
-		self.move_back( (move, self.highest[move]-1) )
+		self.move_back(move)
 		return if_found
 
 	def move(self, move, player):
 		self.board[move][self.highest[move]] = player
-		self.last_move = (move, self.highest[move])
-		self.highest[move] += 1
 
-	def move_back(self, move=None):
-		if not move:
-			move = self.last_move
-		x, y = move
-		self.board[x][y] = ''
-		self.highest[x] -= 1
+	def move_back(self, move):
+		self.board[move][self.highest[move]-1] = ''
 
 
 class FourInLineTreeMinMaxEvaluation:
 	def __init__(self, max_lvl=1, first_state=FourInLine()):
 		self.first_state = first_state
-		self.nodes_to_do = [first_state]
+		self.nodes_to_do = []
 
-		self.PLAYERS = {0: 'W', 1: 'B'}
 		self.MAX_LVL = max_lvl
 		self.create_tree()
-		for _ in range(self.MAX_LVL):
-			self.go_down(self.first_state)
 
 	def create_tree(self):
+		self.expand_node(self.first_state)
 		for node in self.nodes_to_do:
 			self.expand_node(node)
 
 	def expand_node(self, node):
-		current_lvl = node.lvl + 1
-		current_player = self.PLAYERS[current_lvl % 2]
-
-		def create_new_node(top_node, move):
-			new_state = deepcopy(top_node)
-			new_state.next = []
-			new_state.lvl = current_lvl
-			new_state.move(move, current_player)
-			top_node.next.append(new_state)
+		def create_new_node(move):
+			new_state = FourInLine( deepcopy(node.board) )
+			new_state.move(move, new_state.current_player)
+			node.next.append(new_state)
 			return new_state
 
 		def create_new_nodes(top_node):
-			with concurrent.futures.ThreadPoolExecutor() as executor:
-				results = [executor.submit(create_new_node, top_node, move)
-							for move in top_node.possible_moves(current_player)]
-			return [result.result() for result in concurrent.futures.as_completed(results)]
+			with ThreadPoolExecutor() as executor:
+				return executor.map(create_new_node, top_node.possible_moves())
 
-		def add_nodes_to_tiles_to_do_list(nodes):
-			values = [node.value for node in nodes]
-			if 1 not in values and -1 not in values and current_lvl < self.MAX_LVL:
-				for node in nodes:
-					self.nodes_to_do.append(node)
+		for new_node in create_new_nodes(node):
+			self.nodes_to_do.append(new_node)
 
-		nodes = create_new_nodes(node)
-		add_nodes_to_tiles_to_do_list(nodes)
-
-	# update values from bottom to top
-	def go_down(self, game_state):
-		values_next = [game_state.value for game_state in game_state.next]
-		if game_state.value == 0 and None not in values_next:
-			if game_state.lvl % 2 == 0:
-				game_state.value = max(values_next)
+	def print_positions(self, game_state=None, all_=False):
+		if not game_state:
+			game_state = self.first_state
+		if not all_:
+			if not game_state.next:
+				print(game_state)
 			else:
-				game_state.value = min(values_next)
+				for next_tile in game_state.next:
+					self.print_positions(next_tile)
 		else:
-			for next_game_state in game_state.next:
-				self.go_down(next_game_state)
-
-	def print_all_positions(self, game_state=None):
-		if not game_state:
-			game_state = self.first_state
-		print(game_state.demo())
-		for next_state in game_state.next:
-			self.print_all_positions(next_state)
-
-	def board_on_the_bottom(self, game_state=None):
-		if not game_state:
-			game_state = self.first_state
-		if not game_state.next:
 			print(game_state)
-		else:
-			for next_tile in game_state.next:
-				self.board_on_the_bottom(next_tile)
+			for next_state in game_state.next:
+				self.print_positions(next_state)
 
 
 if __name__ == "__main__":
-	import sys
-
-	DEPTH = 2
-	sys.setrecursionlimit(min(10000, 7 ** DEPTH))
+	DEPTH = 3
 
 	tree_search = FourInLineTreeMinMaxEvaluation(DEPTH)
-	# tree_search.print_all_positions()
-	tree_search.board_on_the_bottom()
-	# print(tree_search.first_state.value)
+	tree_search.print_positions(all_=True)
